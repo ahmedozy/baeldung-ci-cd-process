3490221d9131be3339c4ed1c968d995a13ea6ae7
ASTEK <ASTEK@DESKTOP-714PQJU>
2021-11-21 13:48:35 +0300

    Initialize project

---
+++ baeldung-ci-cd-process/src/test/java/sd/wahbi/cicd/BaeldungCiCdProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package sd.wahbi.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeldungCiCdProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
